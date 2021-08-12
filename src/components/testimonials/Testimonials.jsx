7248606658414633ecc3cdb7bd8b1f65f152e0c9
mvdgp <mvandraanen@hotmail.com>
2021-08-12 10:58:34 +0200

    add netlify folder

---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,25 @@
+import "./testimonials.scss"
+
+export default function Testimonials() {
+    return (
+        <div className="testimonials" id="testimonials">
+            <div className="left">
+            <h1>Testimonials</h1>
+            <div className="timpeak">
+                <div className="lefty">
+                    <img src="https://media-exp1.licdn.com/dms/image/C5603AQGEBb_UNOqIhw/profile-displayphoto-shrink_800_800/0/1601532708285?e=1634169600&v=beta&t=QjQmiOpUWx5oJH2mZYMfupn189gyMkNnWhyvsVaBGwQ" alt="Tim de Groot" />
+                </div>
+                <div className="righty">
+                    <div className="testim">
+                    <h2>Tim de Groot</h2>
+                    <h3>Servicedesk Coach at PEAK-IT</h3>
+                    <p>"Manouk is a pleasant colleague to work with. She takes knowledge in quickly and is able to apply this in her own way. Because of her flexible mentality and eagerness to learn she is able to provide a valuable contribution in many areas which makes her a great, indispensable asset to the company."</p>
+                    </div>
+                </div>
+            </div>
+            </div>
+            <div className="right">
+            </div>
+        </div>
+    )
+}
---
+++ src/components/workexp/Workexp.jsx
@@ -0,0 +1,75 @@
+import "./workexp.scss"
+
+export default function Workexp() {
+    return (
+        <div className="workexp" id="workexp">
+            <h1 className="title">Work experience</h1>
+            <div className="wrap">
+            <div className="jobsleft">
+            <div className="solimas">
+                <div className="topSOL">
+                    <h2 className="companySOL">SOLIMAS</h2>
+                    <h3 className="jobSOL">IT SERVICE SPECIALIST</h3>
+                    <h4 className="timeSOL">04-2021 - Present</h4>
+                </div>
+                <div className="bottomSOL">
+                    <p className="explSOL">My responsibility is to provide technical support for a wide array of software and hardware as well as network.</p>
+                </div>
+            </div>
+            <div className="peakit">
+                <div className="topPK">
+                    <h2 className="companyPK">PEAK-IT</h2>
+                    <h3 className="jobPK">SERVICEDESK ENGINEER</h3>
+                    <h4 className="timePK">02-2020 - 04-2021</h4>
+                </div>
+                <div className="bottomPK">
+                    <p className="explPK">My job was to provide technical support for a wide array of software and hardware. I also helped develop the ServiceNow instance for the company.</p>
+                </div>
+            </div>
+            <div className="scotch">
+                <div className="topSC">
+                    <h2 className="companySC">SCOTCH & SODA</h2>
+                    <h3 className="jobSC">STYLIST</h3>
+                    <h4 className="timeSC">03-2018 - 02-2020</h4>
+                </div>
+                <div className="bottomSC">
+                    <p className="explSC">I provided personal styling advice to customers.</p>
+                </div>
+            </div>
+            </div>
+            <div className="jobsright">
+            <div className="werkenik">
+                <div className="topWI">
+                    <h2 className="companyWI">WERK EN IK</h2>
+                    <h3 className="jobWI">SALARY ADMINISTRATOR</h3>
+                    <h4 className="timeWI">02-2016 - 03-2018</h4>
+                </div>
+                <div className="bottomWI">
+                    <p className="explWI">My responsibilities consisted of paying salaries, creating and maintaining contracts, keeping track of collective labour agreement regulations and more.</p>
+                </div>
+            </div>
+            <div className="guerrilla">
+                <div className="topGR">
+                    <h2 className="companyGR">GUERRILLA GAMES</h2>
+                    <h3 className="jobGR">INTERN <span>|</span> VIDEO EDITOR</h3>
+                    <h4 className="timeGR">02-2015 - 08-2016</h4>
+                </div>
+                <div className="bottomGR">
+                    <p className="explGR">I started as a community intern where my work consisted of video editing and production. After my internship I was hired as a video editor at the cinematics department.</p>
+                </div>
+            </div>
+            <div className="diversen">
+                <div className="topDIV">
+                    <h2 className="companyDIV">MISC.</h2>
+                    <h3 className="jobDIV">FRONT DESK <span>|</span> CALL-CENTER <span>|</span> SUPERMARKET</h3>
+                    <h4 className="timeDIV">12-2007 - 02-2015</h4>
+                </div>
+                <div className="bottomDIV">
+                    <p className="explDIV">My previous work experience consists mostly of administrative jobs.</p>
+                </div>
+            </div>
+            </div>
+            </div>
+        </div>
+    )
+}
